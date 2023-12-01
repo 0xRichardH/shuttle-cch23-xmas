04b83f7f4b48e316b9793f8945f8b29836fe1b3b
Richard Hao <richard@0xdev.dev>
2023-12-01 23:03:03 +0800

    feat:  Task 1: Cube the bits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +8,17 @@ async fn fake_error() -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
 }
 
+async fn recalibrate_packet_id(Path((num1, num2)): Path<(u32, u32)>) -> String {
+    let xor_rsult = num1 ^ num2;
+    xor_rsult.pow(3).to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(fake_error));
+        .route("/-1/error", get(fake_error))
+        .route("/1/:num1/:num2", get(recalibrate_packet_id));
 
     Ok(router.into())
 }
