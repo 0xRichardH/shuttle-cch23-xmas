db450dfb698950d9e10f48b96fea44aba222caee
Richard Hao <richard@0xdev.dev>
2023-12-16 17:01:17 +0800

    feat: finish task 3 of day12

--- src/handlers/day12.rs
+++ src/handlers/day12.rs
@@ -5,6 +5,8 @@ use axum::{
     extract::{self, Path, State},
     Json,
 };
+use chrono::{DateTime, Datelike, Utc, Weekday};
+use serde_json::json;
 use ulid::Ulid;
 use uuid::Uuid;
 
@@ -53,7 +55,7 @@ pub async fn convert_ulids_to_uuids(
 pub async fn count_ulids(
     Path(weekday): Path<u8>,
     extract::Json(ulids): extract::Json<Vec<String>>,
-) -> Result<String> {
+) -> Result<Json<serde_json::Value>> {
     if !(0..=6).contains(&weekday) {
         return Err(AppError::BadRequest(format!(
             "Weekday must be between 0 and 6, got {}",
@@ -63,8 +65,32 @@ pub async fn count_ulids(
 
     let ulids = ulids
         .into_iter()
-        .flat_map(|ulid| Ulid::from_string(ulid.as_str()).ok())
-        .collect::<Vec<Ulid>>();
+        .flat_map(|ulid| Ulid::from_string(ulid.as_str()).ok());
 
-    Ok(ulids.len().to_string())
+    let now: DateTime<Utc> = Utc::now();
+    let (mut in_christmas_eve, mut is_weekday, mut in_the_future, mut lsb_is_1) = (0, 0, 0, 0);
+    for ulid in ulids {
+        let datetime: DateTime<Utc> = ulid.datetime().into();
+        if datetime.month() == 12 && datetime.day() == 24 {
+            in_christmas_eve += 1;
+        }
+        if datetime.weekday().num_days_from_monday() == weekday as u32 {
+            is_weekday += 1;
+        }
+        if datetime > now {
+            in_the_future += 1;
+        }
+        let lsb = ulid.to_bytes()[15] & 1;
+        if lsb == 1 {
+            lsb_is_1 += 1;
+        }
+    }
+
+    let result = json!({
+        "christmas eve": in_christmas_eve,
+        "weekday": is_weekday,
+        "in the future": in_the_future,
+        "LSB is 1": lsb_is_1,
+    });
+    Ok(Json(result))
 }
