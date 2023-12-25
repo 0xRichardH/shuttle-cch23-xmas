93e92e2d045cfb3dc4e90832a746a00de5bfb52f
Richard Hao <richard@0xdev.dev>
2023-12-25 15:06:15 +0800

    chore: housekeep

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day04;
 mod day05;
+mod day06;
 mod day07;
 mod day08;
 mod day11;
@@ -16,6 +17,7 @@ mod day21;
 pub use day01::*;
 pub use day04::*;
 pub use day05::*;
+pub use day06::*;
 pub use day07::*;
 pub use day08::*;
 pub use day11::*;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,21 +1,3 @@
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, Error};
-use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
+mod rqwest;
 
-pub struct RequestClient {
-    client: ClientWithMiddleware,
-}
-
-impl RequestClient {
-    pub fn new(max_reties: u32) -> Self {
-        let retry_policy = ExponentialBackoff::builder().build_with_max_retries(max_reties);
-        let client = ClientBuilder::new(reqwest::Client::new())
-            .with(RetryTransientMiddleware::new_with_policy(retry_policy))
-            .build();
-
-        Self { client }
-    }
-
-    pub async fn get(&self, url: &str) -> Result<reqwest::Response, Error> {
-        self.client.get(url).send().await
-    }
-}
+pub use rqwest::*;
